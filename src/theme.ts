63633e96e4a8220105a50b2224b84c9ecb80c011
Michael Bauer <michael@m-bauer.org>
2021-06-17 15:36:12 +0200

    Add missing regular fontweight variant and update theme overrides accordingly.

--- src/theme.ts
+++ src/theme.ts
@@ -7,7 +7,33 @@ export const theme = createMuiTheme({
                 body: {
                     background: 'linear-gradient(70deg, #FFFFFF 50%, #ED7A5D 200%)',
                     backgroundRepeat: "no-repeat",
-                    backgroundAttachment: "fixed",                }
+                    backgroundAttachment: "fixed",
+                }
+            }
+        },
+        MuiTypography: {
+            h1: {
+                fontWeight: 'bold'
+            },
+            h2: {
+                fontWeight: 'bold'
+            },
+            h3: {
+                fontWeight: 'bold'
+            },
+            h4: {
+                fontWeight: 'bold'
+            },
+            h5: {
+                fontWeight: 'bold'
+            },
+            h6: {
+                fontWeight: 'bold'
+            }
+        },
+        MuiButton: {
+            containedPrimary: {
+                fontWeight: 'bold'
             }
         }
     },
