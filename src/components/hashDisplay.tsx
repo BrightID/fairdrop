993fb21f6960a70a0acf739aa1bfa9f077648e4f
Michael Bauer <michael@m-bauer.org>
2021-06-23 12:21:21 +0200

    Add tooltip with full hash to HashDisplay component

--- src/components/hashDisplay.tsx
+++ src/components/hashDisplay.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import {Tooltip} from '@material-ui/core'
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles'
 
 interface HashDisplayProps {
     hash?: string
     className?: string
 }
 
-const HashDisplay = ({hash, className}:HashDisplayProps) => {
-    if (hash)
-        return <span className={className}>{hash.substring(0,5)}...{hash.substring(hash.length-5)}</span>
-    else
+const HashDisplay = ({hash, className}: HashDisplayProps) => {
+    const classes = useStyles()
+    if (hash) {
+        return <Tooltip title={hash} interactive classes={{tooltip: classes.toolTip}}>
+            <span className={className}>{hash.substring(0, 7)}...{hash.substring(hash.length - 4)}</span>
+        </Tooltip>
+    } else {
         return null
+    }
 }
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    toolTip: {
+        maxWidth: 'none',
+        borderRadius: '4px'
+    },
+}),)
+
 
 export default HashDisplay
