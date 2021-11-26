2c6f864fd1181be436799409b8d6621d1b0d4aef
David Wisner <dwisner6@gmail.com>
2021-11-26 16:51:21 +0800

    remove unnecessary file

--- src/calcIncentives.ts
+++
@@ -1,6 +0,0 @@
-// calculate incentiveId
-// const types = ['address', 'address', 'uint256', 'uint256', 'address'];
-// const values = [rewardsTokenAddress, poolAddress, startTime, endTime, refundee];
-// const encodedKey = ethers.utils.defaultAbiCoder.encode(types, values);
-// const incentiveId = ethers.utils.keccak256(encodedKey);
-// console.log(`The new incentiveId will be ${incentiveId}`);
